cabe4e0376052ec07691f25093c9f76dff91d242
KholoodS <sjkholood@gmail.com>
2025-02-12 19:52:10 +0530

    changes

--- backend/config/firebase.js
+++
@@ -1,9 +0,0 @@
-// firebase.js (inside backend/config)
-const admin = require('firebase-admin');
-const serviceAccount = require('./firebaseServiceAccountKey.json');
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
-module.exports = admin;
